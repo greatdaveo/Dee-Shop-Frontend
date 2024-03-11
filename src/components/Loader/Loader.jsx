d3d6c6ee29780a39370481c5a2f9127c90ba3382
David Olowomeye <olowomeyedavid242@gmail.com>
2024-03-11 20:44:48 +0000

    Created a simple loading component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../styles/components/loader/loader.css";
+import loaderImg from "../../assets/components/loader.gif";
+
+const Loader = () => {
+  return (
+    <div className="loading-wrapper">
+      <div className="loader">
+        <img src={loaderImg} alt="Loading..." />
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
