78675c776ca1778d1a5c6e3f1f8b7fc959e8d951
David Olowomeye <olowomeyedavid242@gmail.com>
2024-03-27 10:44:37 +0000

    Created the Category Redux Logic

---
+++ src/redux/features/CategoryAndBrands/CategoryAndBrandSlice.js
@@ -0,0 +1,67 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+import categoryAndBrandService from "./categoryAndBrandService";
+
+const initialState = {
+  category: [],
+  isError: false,
+  isLoading: false,
+  isSuccess: false,
+  message: "",
+};
+
+// For Create Category
+export const createCategorySlice = createAsyncThunk(
+  "category/create-category",
+  async (formData, thunkApi) => {
+    try {
+      return await categoryAndBrandService.createCategory(formData);
+    } catch (error) {
+      const message =
+        (error.message && error.response.data && error.message.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+export const categoryAndBrandSlice = createSlice({
+  name: "category",
+  initialState,
+  reducers: {
+    RESET_CAT(state) {
+      state.isError = false;
+      state.isSuccess = false;
+      state.isLoading = false;
+      state.message = "";
+    },
+  },
+
+  extraReducer: (builder) => {
+    builder
+      // For Create Category
+      .addCase(createCategorySlice.pending, (state) => {
+        state.isLoading = true;
+      })
+
+      .addCase(createCategorySlice.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.isError = false;
+        toast.success("Category has been created successfully!");
+      })
+
+      .addCase(createCategorySlice.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        toast.error(action.payload);
+      });
+  },
+});
+
+export const { RESET_CAT } = categoryAndBrandSlice.actions;
+
+export default categoryAndBrandSlice.reducer;
---
+++ src/redux/features/CategoryAndBrands/categoryAndBrandService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const Backend_Url = import.meta.env.VITE_Backend_Url;
+export const API_URL = `${Backend_Url}/api/`;
+
+// For Create Category
+const createCategory = async (formData) => {
+  const response = await axios.post(
+    API_URL + "category/create-category",
+    formData
+  );
+  return response.data;
+};
+
+const categoryAndBrandService = {
+  createCategory,
+};
+
+export default categoryAndBrandService;
