1eca216310408d67cbd4e4b139f5449cff9002ee
David Olowomeye <olowomeyedavid242@gmail.com>
2024-04-15 06:29:57 +0100

    Started the feature to filter by category name

--- src/components/shop/ShopProductFilter.jsx
+++ src/components/shop/ShopProductFilter.jsx
@@ -1,10 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/components/shop/ShopProductFilter.css";
+import { useSelector } from "react-redux";
 
 const ShopProductFilter = () => {
+  const { products, minPrice, maxPrice } = useSelector(
+    (state) => state.product
+  );
+
+  const [category, setCategory] = useState("All");
+  const allCategories = [
+    "All",
+    // this will create a new set and it will contain the list of the product category
+    ...new Set(products?.map((product) => product.category)), // new Set() method will make a duplicate category one
+  ];
+  // console.log(allCategories);
+
+  const filterProductsByCategory = (cat) => {
+    console.log(cat);
+  };
+
   return (
     <div className="shop-filter">
-      <h3>Product Filter</h3>
+      <h2>Categories</h2>
+
+      <div>
+        {allCategories.map((cat, i) => {
+          return (
+            <h4
+              key={i}
+              type="button"
+              className={`${category}` === cat ? "isActive" : "mull"}
+              onClick={() => filterProductsByCategory(cat)}
+            >
+              &#8250; {cat}
+            </h4>
+          );
+        })}
+      </div>
     </div>
   );
 };
