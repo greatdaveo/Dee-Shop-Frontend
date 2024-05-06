c5540694c3fdf6637b6308098d59c94a034e3f65
David Olowomeye <olowomeyedavid242@gmail.com>
2024-05-06 16:38:50 +0100

    Added the content of  CheckoutSuccess page

--- src/pages/checkout/CheckoutSuccess.jsx
+++ src/pages/checkout/CheckoutSuccess.jsx
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "../../components/homepage/NavBar";
 import Footer from "../../components/Footer";
+import { Link } from "react-router-dom";
+import Confetti from "react-confetti";
+import { useDispatch } from "react-redux";
+import { CLEAR_CART } from "../../redux/features/cart/cartSlice";
 
 const CheckoutSuccess = () => {
+  const dispatch = useDispatch();
+
+  // To clear cart when the user checkout successfully
+  useEffect(() => {
+    dispatch(CLEAR_CART());
+  }, [dispatch]);
+
   return (
     <div>
+      <Confetti />
+
       <div style={{ background: "black" }}>
         <NavBar />
       </div>
-      <h1>Checkout Success</h1>
+
+      <div style={{ padding: "4rem", height: "50vw" }}>
+        <h1>Checkout Successful! 😁👍</h1>
+        <p>Thank you for your purchase!</p>
+
+        <button
+          style={{
+            padding: "0.5rem 2rem",
+            border: "none",
+            backgroundColor: "gold",
+          }}
+        >
+          <Link
+            to="/order-history"
+            style={{
+              color: "black",
+              fontWeight: "bold",
+            }}
+          >
+            View Order Status
+          </Link>
+        </button>
+      </div>
 
       <Footer />
     </div>
